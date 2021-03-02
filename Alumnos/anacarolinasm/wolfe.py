6602bf0c9d1a6f6d72ddd83fdff9be1a98526ca3
Ana Sandoval <anasandoval@MacBook-Air-de-soporte.local>
2021-03-01 20:04:28 -0600

    Actualización Wolfe

--- Alumnos/anacarolinasm/wolfe.py
+++ Alumnos/anacarolinasm/wolfe.py
@@ -1,10 +1,14 @@
 import numpy as np 
 from derivadas import cuadrados
 
-def condiciones_wolfe(f, x0, alpha, pk, tol=1e-5):
-    phi = lambda alpha : f(x0 + alpha*pk)
-    return phi(3)
+from derivadas import Grad,Hess
 
+def condiciones_wolfe(f, x0, alpha, pk, tol=1e-5):
+    phi = lambda alpha : f(x0 + alpha*pk) #lambda es una función in-line (para simplificar la definición de la función y para poder evaluar más fácilmente)
+    linea = lambda alpha : f(x0) + c1*alpha*Grad(x0)*pk
+    return linea(alpha) - phi(alpha) >= 0 and Grad(x0+alpha*pk)*pk-c2*Grad(x0)*pk >=0
+    #falta transponer 
 
 if __name__ == '__main__':
     print(condiciones_wolfe(cuadrados, [1,1,1,1], 1, [-1,-1,-1,-1]))
+
