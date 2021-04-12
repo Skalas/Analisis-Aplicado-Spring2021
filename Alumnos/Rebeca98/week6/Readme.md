9f2b011ca3534fd3f5057c113654cb4033d6e5e0
Rebeca Angulo Rojas <rbbkitanias@gmail.com>
2021-04-12 12:15:50 -0500

    borre carpetas en mi folder

--- Alumnos/Rebeca98/week6/Readme.md
+++
@@ -1,4 +0,0 @@
-# Tarea para el miércoles: 
-- Implementar el cambio para que la hessiana sólo calcule la mitad de las entradas. 
-- Implementar condiciones de segundo orden
-
--- Alumnos/Rebeca98/week8/busqueda_lineal.py
+++
@@ -1,7 +0,0 @@
-"""Implementar algoritmo de búsqueda lineal"""
-### x0 definido
-### while no se cumplen de paro()
-    ### calcular pk
-        ### para esto resolvemos el sistema lineal de la Hessiana*pk- gradiente (en caso de ser positiva def)
-        ### en caso de no ser positiva definida, la hacemos positiva definida
-    ### xk+1 = xk + alphak * pk
