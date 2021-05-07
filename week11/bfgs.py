16e3f5a69de1a2b79513052c1f9d88ef155da682
Miguel Escalante <miguel.escalante@itam.mx>
2021-05-06 23:29:02 -0500

    Update to method

--- week11/bfgs.py
+++ week11/bfgs.py
@@ -1,55 +1,33 @@
 import numpy as np
+from numpy import linalg as LA
+from derivadas import Grad, cuadrados, rosenbrock, Hess
+from wolfe import genera_alpha
+from act_bfgs import BFGS_Hk, DFP_Hk
 
 
-def DFP_BK(yk, sk, Bk):
-    """
-    Función que calcula La actualización DFP de la matriz Bk
-    In:
-      yk: Vector n
-      sk: Vector n
-      Bk: Matriz nxn
-    Out:
-      Bk+1: Matriz nxn
-    """
-    n = len(yk)
-    rhok = 1 / (yk.T*sk)
-    Vk = (np.eye(n) - rhok * yk*sk.T)
-    Bk1 = Vk * Bk * Vk.T + rhok * yk * yk.T
-    return Bk1
-
-
-def DFP_HK(yk, sk, Hk):
-    """
-    Función que calcula La actualización DFP de la matriz Hk
-    In:
-      yk: Vector n
-      sk: Vector n
-      Hk: Matriz nxn
-    Out:
-      Hk+1: Matriz nxn
-    """
-    Hk1 = Hk - (Hk * yk * yk.T * Hk)/(yk.T * Hk * yk) + (sk * sk.T)/(yk.T * sk)
-    return Hk1
-
-
-def BFGS_HK(yk, sk, Hk):
-    """
-    Función que calcula La actualización BFGS de la matriz Hk
-    In:
-      yk: Vector n
-      sk: Vector n
-      Hk: Matriz nxn
-    Out:
-      Hk+1: Matriz nxn
-    """
-    n = len(yk)
-    rhok = 1 / (yk.T*sk)
-    Vk = (np.eye(n) - rhok * yk * sk.T)
-    Hk1 = Vk.T * Hk * Vk + rhok * sk * sk.T
-    return Hk1
-
-## BFGS Bk
+def BFGS(f, x0, tol, H0, maxiter=10000):
+    k = 0
+    Gk = Grad(f, x0)
+    Hk = H0
+    xk = np.array(x0)
+    xk1 = np.array(x0)
+    sk = np.array(100)
+    while (LA.norm(Gk) > tol and LA.norm(sk) > tol and k <= maxiter):
+        pk = - Hk.dot(Gk)
+        alphak = genera_alpha(f, xk, pk)
+        xk1 = xk + alphak * pk
+        sk = xk1 - xk
+        Gk1 = Grad(f, xk1)
+        yk = Gk1 - Gk
+        Hk = BFGS_Hk(yk, sk, Hk)
+        k += 1
+        xk = xk1
+        Gk = Gk1
+    return xk1, k
 
 
 if __name__ == "__main__":
-    print("")
+    x, k = BFGS(rosenbrock, np.array([-100000, 1]), 1e-15, np.eye(2))
+    print(f'Llegué a {x} en {k} iteraciones')
+    x, k = BFGS(cuadrados, np.array([-10, 10, 20, 100]), 1e-15, np.eye(4))
+    print(f'Llegué a {x} en {k} iteraciones')
